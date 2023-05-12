90f3a5e868d024271d3a549678db325f6456b6f2
Nicolas Soares Martins <n3c.soares@gmail.com>
2023-05-12 16:14:59 -0300

    first commit

---
+++ src/MaiorNumero.java
@@ -0,0 +1,12 @@
+public class MaiorNumero {
+    public static int encontrarMaior(int[] numeros) {
+        int maior = numeros[0];
+        for (int i = 1; i < numeros.length; i++) {
+            if (numeros[i] > maior) {
+                maior = numeros[i];
+            }
+        }
+        return maior;
+    }
+}
+
